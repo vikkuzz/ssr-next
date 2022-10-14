fdc994783329b9fb41dbc86549fc95325cd085fa
Vikkuzz <vikkuzz@vk.com>
2022-10-14 10:35:10 +0300

    footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,9 +1,35 @@
+import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
 
 import styles from "../../styles/Footer.module.scss";
 
 const Footer = () => {
-  return <div className={`${styles.footer}`}>Footer</div>;
+  const locale = useSelector((state) => state.royalfutReducer.locale);
+
+  return (
+    <div className={`${styles.footer}`}>
+      <div className={`${styles.footer_firstcolumn}`}>
+        <div className={`${styles.footer_header}`}>ОБЩАЯ ИНФОРМАЦИЯ</div>
+        <div className={`${styles.footer_infowrapper}`}>
+          <div
+            className={`${styles.footer_info_item} ${styles.footer_info_font}`}
+          >
+            Yasha Limited, Office 908H Nicolaou Pentadromos Center, Limassol,
+            Cyprus
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.footer_secondcolumn}`}>
+        <div className={`${styles.footer_header}`}>ПЛАТФОРМЫ</div>
+        <div className={`${styles.footer_infowrapper}`}></div>
+      </div>
+      <div className={`${styles.footer_thirdcolumn}`}>
+        <div className={`${styles.footer_header}`}>ЮРИДИЧЕСКАЯ ИНФОРМАЦИЯ</div>
+        <div className={`${styles.footer_infowrapper}`}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
