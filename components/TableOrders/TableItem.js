41e3a74bbf6d564d55e54deaeeb26caefe676d3d
Vikkuzz <vikkuzz@vk.com>
2022-12-01 00:44:13 +0300

    scroll

--- components/TableOrders/TableItem.js
+++ components/TableOrders/TableItem.js
@@ -37,8 +37,25 @@ const TableItem = ({ item }) => {
         setDay(currentDay);
     }, []);
 
-    const handleClickOrder = () => {
+    const handleClickOrder = (e) => {
         setOrdeOpen(!orderOpen);
+        function getCoords(elem) {
+            // crossbrowser version
+            var box = elem.getBoundingClientRect();
+            var body = document.body;
+            var docEl = document.documentElement;
+            var scrollTop =
+                window.pageYOffset || docEl.scrollTop || body.scrollTop;
+            var scrollLeft =
+                window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+            var clientTop = docEl.clientTop || body.clientTop || 0;
+            var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+            var top = box.top + scrollTop - clientTop;
+            var left = box.left + scrollLeft - clientLeft;
+            return { top: Math.round(top), left: Math.round(left) };
+        }
+        console.log(getCoords(e.target));
+        window.scrollTo(0, getCoords(e.target).top - 200);
     };
 
     return (
@@ -112,6 +129,11 @@ const TableItem = ({ item }) => {
                         {item.status}
                     </div>
                 </div>
+                <div
+                    className={`${styles.tableorder_arrow} ${
+                        orderOpen && styles.tableorder_arrow_trasform
+                    }`}
+                ></div>
             </div>
             <div
                 className={`${styles.tableorder_content} ${
